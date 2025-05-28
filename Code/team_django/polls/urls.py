200fafaed887170c09b863329451df00f85a2fca
hyemi0622 <hyemi8292@naver.com>
2025-05-28 23:00:01 +0900

    Update urls.py

--- Code/team_django/polls/urls.py
+++ Code/team_django/polls/urls.py
@@ -3,13 +3,11 @@
 from django.urls import path
 from . import views  # ← 꼭 이렇게 상대 경로로 import
 
-
-   
-   
-    
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('chat/', views.chat, name='chat'),
-
-
+    path('fourpanel/', views.fourpanel, name='fourpanel'),
+    path('memorygame/', views.memorygame, name='memorygame'),
+    path('otherusers/', views.otherusers, name='otherusers'),
 ]
+
