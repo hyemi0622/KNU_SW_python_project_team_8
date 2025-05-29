71df87e23190e1472151a44a657fdaf8a4569959
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2025-05-29 10:17:57 +0900

    기존 team_django 삭제 후 새 프로젝트 추가

--- Code/team_django/polls/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Question
-
-@admin.register(Question)
-class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('category', 'subcategory', 'order', 'text')
-    list_filter = ('category', 'subcategory')
-    search_fields = ('text',)
