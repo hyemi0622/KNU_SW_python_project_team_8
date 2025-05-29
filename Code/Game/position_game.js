0a38f784a320221bf91e167c50879952ea221545
hemkong2 <gohm9464@naver.com>
2025-05-29 15:44:10 +0900

    Update position_game.js

--- Code/Game/position_game.js
+++ Code/Game/position_game.js
@@ -9,7 +9,7 @@ let foundTargets = [];
 fetch('/static/data/position_game_levels.json')
   .then(res => res.json())
   .then(data => {
-    const levelData = data.find(l => l.level === 1); // 현재 1단계만
+    const levelData = data.find(l => l.level === 1); 
     image.src = `/static/${levelData.background}`;
     targets = levelData.targets;
     foundTargets = Array(targets.length).fill(false);
@@ -22,9 +22,9 @@ fetch('/static/data/position_game_levels.json')
 
 canvas.addEventListener('click', function (e) {
   const rect = canvas.getBoundingClientRect();
-  const x = (e.clientX - rect.left).toFixed(2);
-  const y = (e.clientY - rect.top).toFixed(2);
-  console.log(` 클릭 좌표: (${x}, ${y})`);
+  const x = parseFloat(e.clientX - rect.left).toFixed(2);
+  const y = parseFloat(e.clientY - rect.top).toFixed(2);
+  console.log(`클릭 시 좌표: (${x}, ${y})`);
 
   for(let i =0; i<targets.length;i++){
     if(!foundTargets[i]){
@@ -39,13 +39,21 @@ canvas.addEventListener('click', function (e) {
       } else{
         const dx = x - target.x;
         const dy = y - target.y;
-        const distance = Math.sqrt(dx * dy + dy*dy);
+        const distance = Math.sqrt(dx * dx + dy*dy);
         isCorrect = distance < target.radius;
       }
       if (isCorrect){
         foundTargets[i] = true;
         renderWordList();
+
+        if('xRange' in target && 'yRange' in target){
+          const cx = (target.xRange.min + target.xRange.max) / 2;
+          const cy = (target.yRange.min + target.yRange.max) /2;
+
+          renderCircle({x : cx, y : cy, radius: target.reaius ||30 });
+        } else{
         renderCircle(target);
+        }
         checkAllFound();
         break;
       }
