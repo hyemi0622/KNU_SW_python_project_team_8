552f793081a6094bfd5cbf3a7c14a004aa342765
hyemi0622 <hyemi8292@naver.com>
2025-06-01 16:50:26 +0900

    Update urls.py

--- Code/team_django/polls/urls.py
+++ Code/team_django/polls/urls.py
@@ -8,8 +8,12 @@ from . import views  # ← 꼭 이렇게 상대 경로로 import
    
     
 urlpatterns = [
-    path('index/', views.index, name='index'),
+     path('index/', views.index, name='index'),
     path('chat/', views.chat, name='chat'),
+    path('fourpanel/', views.fourpanel, name='fourpanel'),
+    path('memorygame/', views.memorygame, name='memorygame'),
+    path('otherusers/', views.otherusers, name='otherusers'),
+    path('meetourteam/', views.meetourteam, name='meetourteam'),
     path('get_followup_questions/', views.get_followup_questions, name='get_followup_questions'),
     path("process_answers/", views.process_answers, name="process_answers"),  
 
